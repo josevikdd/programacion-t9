e881fd3609ee33c0b0a46ec48d2faf93190dd607
José Vicente Sánchez Vargues <josevi97sgb@gmail.com>
2026-05-27 22:35:00 +0200

    Añadir las dependences necesarias.

--- programacion-t9a5.JoseVicenteSanchezVargues/src/main/java/controller/CapturaDatosVideoClub.java
+++ programacion-t9a5.JoseVicenteSanchezVargues/src/main/java/controller/CapturaDatosVideoClub.java
@@ -1,4 +1,16 @@
 package controller;
 
-public class CapturaDatosVideoClub {
+import entities.Alquiler;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import javax.servlet.ServletException;
+
+@WebServlet("/CapturaDatosVideoClub")
+public class CapturaDatosVideoClub extends HttpServlet {
+    
 }
